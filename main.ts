009428585df8a049e8dfb7cad253bfa84ffbb235
Maria-Varghese <maria.varghese@mths.ca>
2024-11-01 08:17:34 -0700

    Program uses sonar and neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,56 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria Varghese
+ * Created on: Sep 2024
+ * This program uses neopixels and sonar
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+//setup
+basic.showIcon(IconNames.Diamond)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.pause(1000)
+
+//finding distance from sonar
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    basic.showNumber(distanceToObject)
+    basic.showString(' cm')
+    basic.showIcon(IconNames.Happy)
+
+    if (distanceToObject < 10) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(1000)
+    } else {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.pause(1000)
+    }
+})
+
+//clearScreen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
